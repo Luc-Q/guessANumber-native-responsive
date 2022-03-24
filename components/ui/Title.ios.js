39064afdfc7717a8a97dd0640c52e9125f8ea085
Lu <jetqin_2008@hotmail.com>
2022-03-24 18:19:13 +1100

    platform styles

--- components/ui/Title.ios.js
+++ components/ui/Title.ios.js
@@ -15,7 +15,6 @@ const styles = StyleSheet.create({
         textAlign: "center",
         // borderWidth: Platform.OS === "android" ? 2 : 0,
         // borderWidth: Platform.select({ ios: 0, android: 2 }),
-        borderColor: "white",
         padding: 12,
         maxWidth: "80%",
         width: 300,
